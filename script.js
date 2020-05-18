ac9fd8c22160f5952cdd6721b9afe7e2a6c27c61
Askat <example@gmail.com>
2020-05-18 14:58:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*let number = 5;
+const  leftBorderWidth = 1;
+
+
+number = 10;
+console.log(number);
+
+//alert('Hello');
+
+const result = confirm("Are you here?");
+console.log = (result);*/
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your name?', '');
+answers[2] = prompt('What is your name?', '');
+
+document.write(answers);
+
